b02dfce309e3c84f536f05c9c8e19eb3c01ca9c6
Sandeep-508 <elsenothing321@gmail.com>
2024-05-22 23:43:18 +0530

    compled the page with scroll to top

--- assets/js/app.js
+++ assets/js/app.js
@@ -68,3 +68,23 @@ window.addEventListener("blur", () => {
 window.addEventListener("focus", () => {
     document.title = title;
 });
+
+// scroll to top
+
+let scroll_btn = document.querySelector(".scroll_top");
+let scroll_pt = 200;
+
+scroll_btn.classList.add("d-none");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > scroll_pt) {
+        scroll_btn.classList.remove("d-none");
+    } else {
+        scroll_btn.classList.add("d-none");
+    }
+    scroll_btn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
+});
